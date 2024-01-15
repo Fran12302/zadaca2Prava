6d8d35f55360df74348d462a5c029bcb283f761b
Fran12302 <156135106+Fran12302@users.noreply.github.com>
2024-01-15 19:11:03 +0100

    Add files via upload

---
+++ SRPProgram.cs
@@ -0,0 +1,34 @@
+﻿namespace zadaca_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        public class Product
+        {
+            public string Name { get; set; }
+            public float TaxRate { get; set; }
+            public float Price { get; set; }
+
+            
+            public Product(string name, float  taxRate, float price)
+            {
+                Name = name;
+                TaxRate = taxRate;
+                Price = price;
+            }
+
+            public float Calculate_Tax()
+            {
+                return Price * TaxRate;
+            }
+            //Klasa produkt treba imati samo jednu odgovornost, da stvori produkt... Calculate Tax trebamo prebaciti u novu klasu npr. Tax_Calculator
+
+
+        }
+    }
+}
